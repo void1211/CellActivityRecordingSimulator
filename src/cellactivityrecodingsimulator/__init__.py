8cd08940f1aee225b396d0b8917a3d0cdc0342c5
void1211 <g2154488@cc.kyoto-su.ac.jp>
2025-10-02 14:15:11 +0900

    change package name

--- src/cellactivityrecodingsimulator/__init__.py
+++ src/cellactivityrecodingsimulator/__init__.py
@@ -15,7 +15,7 @@ from .tools import addSpikeToSignal, make_save_dir
 from .main import main, run
 from .carsIO import load_settings_file, load_cells_from_json, load_sites_from_json, save_data, load_noise_file, load_spike_templates
 
-__version__ = importlib.metadata.version("cellactivityrecodingsimulater")
+__version__ = importlib.metadata.version("cellactivityrecodingsimulator")
 
 # __all__ = [
 #     'BaseObject',
