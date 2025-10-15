7b38d0c753e28c2ff8d1cb77b9bcd10e4b8615ae
void1211 <g2154488@cc.kyoto-su.ac.jp>
2025-10-15 14:54:06 +0900

    change version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cellactivityrecodingsimulator",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(include=['cellactivityrecodingsimulator*']),
     install_requires=[
         'typing-extensions>=4.10.0',
